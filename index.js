45684ed21cdcf34ee84ddf37cb7f4a814b446dc8
scauzml <13828607279@163.com>
2019-06-10 19:23:50 +0800

    张明亮提交

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// 阅读推荐的链接，复习JavaScript中数组的相关方法
 
 // 写程序判断下列变量是不是数组类型。
-var a = '[a, b, c, d]';
+var a2 = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+console.log(Array.isArray(a2));
+console.log(Array.isArray(b));
 
-
-// 编写程序，将下面数组中的每一项都乘以2。
-var a = [1, 2, 3, 4, 5];
-// TODO should output [2,4,6,8,10]
 
 
 // 编写程序，按下面的要求输出结果。
@@ -16,13 +13,50 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
-
+console.log(colors.reduce(function(pre,cur,index,colors){
+    return pre+" "+cur;
+}) )
+console.log(colors.reduce(function(pre,cur,index,colors){
+    return pre+"+"+cur;
+}) )
+console.log(colors.reduce(function(pre,cur,index,colors){
+    return pre+","+cur;
+}) )
 // 编写程序，将下面数组中的数字按从大到小的顺序排序。
-var a = [5, 1, 8, 10, 4];
+var a1 = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+a1.sort();
+a1.reverse();
+console.log(a1);
 
 // 编程程序，找出下列数组中出现频率最高的元素。
-var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var a3 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+//记录key
+var key=new Array;
+//记录value
+var map=new Map();
+a3.forEach(element => {
+ if(!key.includes(element)){
+     key.push(element);
+   var count=0;
+   a3.forEach(e=>{
+       if(element==e){
+           count++;
+       }
+   })
+   map.set(element,count);
+ }   
+});
+
+var result;
+var max=0;
+
+function logMapElements(value, key, map) {
+    if(value>max){
+        max=value;
+        result=key;
+    }
+}
+map.forEach(logMapElements);
+console.log(result);
